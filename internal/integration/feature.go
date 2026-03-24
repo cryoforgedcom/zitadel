672e4bc9f3531192b035f8220607e904000abc91
Tim Möhlmann <tim+github@zitadel.com>
2026-03-24 06:35:04 +0000

    test: remove unneeded GetInstanceFeature calls

--- internal/integration/feature.go
+++ internal/integration/feature.go
@@ -3,7 +3,6 @@ package integration
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/muhlemmer/gu"
 	"github.com/stretchr/testify/assert"
@@ -16,18 +15,6 @@ func EnsureInstanceFeature(t *testing.T, ctx context.Context, instance *Instance
 	ctx = instance.WithAuthorizationToken(ctx, UserTypeIAMOwner)
 	_, err := instance.Client.FeatureV2.SetInstanceFeatures(ctx, features)
 	require.NoError(t, err)
-	retryDuration, tick := WaitForAndTickWithMaxDuration(ctx, 5*time.Minute)
-	require.EventuallyWithT(t,
-		func(tt *assert.CollectT) {
-			got, err := instance.Client.FeatureV2.GetInstanceFeatures(ctx, &feature.GetInstanceFeaturesRequest{
-				Inheritance: true,
-			})
-			require.NoError(tt, err)
-			assertFeatures(tt, got)
-		},
-		retryDuration,
-		tick,
-		"timed out waiting for ensuring instance feature")
 }
 
 type RelationalTableFeatureMatrix struct {
